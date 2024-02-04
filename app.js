4ca1d9fbf512fd20e11d33bbd37877701e1291e2
Satyajit <thatssatya@gmail.com>
2024-02-05 00:44:47 +0530

    Added middleware for verifying origin

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
 app.use(bodyParser.json());
-app.use(cors({ origin: process.env.allowedOrigin }));
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin || req.headers.referer;
+    if (origin && origin.startsWith(process.env.allowedOrigin)) {
+        console.log(`Accessing from: ${origin}`)
+        next();
+    } else {
+        console.log(`Request forbidden from ${origin} origin`);
+        res.status(403).send();
+    }
+});
 
 app.use('/', routes);
 
