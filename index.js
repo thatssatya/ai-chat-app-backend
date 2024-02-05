63704a5615949248e4262c953699e5d41c038110
Satyajit <thatssatya@gmail.com>
2024-02-05 12:50:29 +0530

    Moved index.js to root

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const port = process.env.port;
-const app = require('./app')
+const app = require('./src/app')
 
 app.listen(port, () => {
     console.log(`Backend server listening at http://localhost:${port}`);
